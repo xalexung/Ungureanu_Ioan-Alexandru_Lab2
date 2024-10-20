4bc28de975fca1991b9face20a74f6c5d88a7e2f
Alex <ioanalexandruungureanu@gmail.com>
2024-10-20 17:30:00 +0300

    Add project files.

---
+++ Models/Publisher.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ungureanu_Ioan_Alexandru_Lab2.Models
+{
+    public class Publisher
+    {
+        public int ID { get; set; }
+        [Display(Name = "Publisher Name")]
+        public string PublisherName { get; set; }
+        public ICollection<Book>? Books { get; set; }
+    }
+}
