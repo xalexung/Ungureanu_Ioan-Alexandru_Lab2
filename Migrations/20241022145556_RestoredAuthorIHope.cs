fd63f1c239253c92d461f33a40d530b4ef51a0bd
Alex <ioanalexandruungureanu@gmail.com>
2024-10-22 18:00:48 +0300

    Laborator3

---
+++ Migrations/20241022145556_RestoredAuthorIHope.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ungureanu_Ioan_Alexandru_Lab2.Migrations
+{
+    /// <inheritdoc />
+    public partial class RestoredAuthorIHope : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
